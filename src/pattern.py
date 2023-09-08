f79a8e17ae3391a8c11bd338c4b6425c655d4b95
Bjyrk <143783464+Bjyrk@users.noreply.github.com>
2023-09-08 10:03:57 +0200

    test

--- src/pattern.py
+++ src/pattern.py
@@ -1,2 +1,17 @@
 
 # Print the pattern
+
+start ='*'
+
+for i in range(9):
+    if i == 0:
+            print(start)
+    elif i <= 4:
+        ny = ' *'
+        mønster = start+ny
+        start = mønster
+        print(mønster)
+    elif i > 4:
+        mindre = 9-(i*2)
+        mindre_mønster = mønster[0:mindre]
+        print(mindre_mønster)
--- src/print-1-10-growing.py
+++ src/print-1-10-growing.py
@@ -1,2 +1,14 @@
 
 # Print the numbers described in the exercise
+
+tal=''
+
+for i in range(10):
+    ny = str(i+1)
+    if i == 0:
+        svar = tal+ny
+        tal = svar
+    else:
+        svar = tal+' '+ny
+        tal = svar
+    print(svar)
